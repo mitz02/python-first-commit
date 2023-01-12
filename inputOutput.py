3658ea76154d386e6d621a39188cf72576ebd3fb
mitz02 <odogoedwin@gmail.com>
2023-01-12 07:25:09 -0800

    first commit

---
+++ inputOutput.py
@@ -0,0 +1,14 @@
+import json
+filename = 'userName.json'
+name = ''
+# Check for a history file try:
+try:
+   with open(filename, 'r') as r: # Load the user's name from the history file
+           name = json.load(r)
+
+except IOError:
+    print("First-time login")
+# If the user was found in the history file, welcome them back if name != "": print("Welcome back, " + name + "!")
+else: # If the history file doesn't exist, ask the user for their name
+     name = input("Hello! What's your name? ")
+     print("Welcome, " + name + "!")
